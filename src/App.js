b9f238ec343bbe5599294c7a059f11ee0a0ba697
AYUSH <ayushrajbls@gmail.com>
2023-09-02 18:23:55 +0530

    added footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css"
 import Domain1 from "./components/Domain1"
+import Footer from "./components/Footer/Footer"
 function App() {
   return (
     <div className="App">
       <Domain1 />
+      <Footer/>
     </div>
   )
 }
